2a06dd1ed0d106d31b6783f5b92b8373f9fa20ea
trist <trist@DESKTOP-TDMHE05>
2019-12-16 14:12:12 +0100

    Premier ajout de projet java

---
+++ GestionRdvPsy/src/application/TestMain.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		try (Connection conn = DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:1521:xe", "", "")) {
+			if (conn != null) {
+				System.out.println("Connected to the database!");
+			} else {
+				System.out.println("Failed to make connection!");
+			}
+
+		} catch (SQLException e) {
+			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
